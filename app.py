a2883802a2d4d7645114aaf1702a35926ce1f050
Luis Yao <yaoyangluis@gmail.com>
2019-09-08 23:01:51 -0500

    changed from flask jwt to flask jwt extended.

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -16,14 +15,17 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'luis'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
-        
+# jwt = JWT(app, authenticate, identity) # create /Auth endpoint
+jtw = JWTManager(app) # Does not create /auth endpoint    
+    
 api.add_resource(StoreList, '/stores')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items') 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
+
 
 if __name__ == '__main__':
     from db import db
